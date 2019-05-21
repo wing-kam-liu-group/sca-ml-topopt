828369e9cfc20344404ffd7df2b425a924c91510
Orion Kafka <olkafka@u.northwestern.edu>
2019-05-20 21:13:10 -0500

    Updated CNN.m with correct license terms

--- TOPOPT/CNN.m
+++ TOPOPT/CNN.m
@@ -1,4 +1,22 @@
 %%%%%%%%%% CNN+FFNN constraint %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     This program is for Training the FFNN 
+%     Copyright (C) May 20 2019,  Hengyang Li, Northwestern University
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+% 
+%     Last Modifyed: May 20, 2019
+
 function [xnew]=CNN(nelx,nely,x,yib)  
 xnew=x;
 global net;

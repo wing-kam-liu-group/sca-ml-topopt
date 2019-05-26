21743f34ebc7261f1377043e0789bb05a6f5e1f1
jgr <jiayinggao2016@u.northwestern.edu>
2019-05-26 00:42:21 -0500

    Add files via upload

--- CNN/CNN_train.m
+++ CNN/CNN_train.m
@@ -18,8 +18,8 @@
 function main
 global CNN_hist
 %%load inputs
-load('CNN_train_in_git.mat');
-load('CNN_validation_in_git.mat');
+load('..\Database\CNN_train_in_git.mat');
+load('..\Database\CNN_validation_in_git.mat');
 sample_size=size(sig_local_train,4);
 rand_seq=randperm(sample_size);
 XTrain=sig_local_train(:,:,:,rand_seq);

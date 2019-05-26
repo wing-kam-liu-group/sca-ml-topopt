5608325dd2fdddf0ed3ecddd3b477f331283e2ef
Orion Kafka <olkafka@u.northwestern.edu>
2019-05-26 09:43:17 -0500

    Update CNN_Error_plot.m

--- CNN/CNN_Error_plot.m
+++ CNN/CNN_Error_plot.m
@@ -17,16 +17,17 @@
 
 %% predict macroscopic strain state
 load('CNN_results.mat')
-eps_macro_prediction=predict(net,sig_local_validation,'ExecutionEnvironment','auto');
+eps_macro_prediction=predict(net, sig_local_validation, 'ExecutionEnvironment', 'auto');
 
 % l2 norm 
-Diff=eps_macro_prediction-eps_macro_validation;
+Diff = eps_macro_prediction-eps_macro_validation;
 
 l1=sum(abs(Diff),2);
 l2=sum((Diff).^2,2);
 hist(l2,40);
 set(gca,'FontSize',16);
-xlabel(['Difference in ',char(8467),'^{2} norm'],'fontsize',16); ylabel('Number of Samples','fontsize',16);
+xlabel(['Difference in ',char(8467),'^{2} norm'], 'fontsize', 16);
+ylabel('Number of Samples', 'fontsize', 16);
 ax = gca;
 ax.FontSize = 16;
 
@@ -35,40 +36,60 @@ ax.FontSize = 16;
 fs=16;
 figure; 
 subplot(1,3,1);
-plot(eps_macro_prediction(:,1),eps_macro_validation(:,1),'r*');hold on
+plot(eps_macro_prediction(:,1),eps_macro_validation(:,1),'r*');
+hold on
 plot(eps_macro_validation(:,1),eps_macro_validation(:,1),'k')
-cor=corrcoef(eps_macro_prediction(:,1),eps_macro_validation(:,1));
-str=sprintf('r= %1.4f',cor(1,2));
+
+cor = corrcoef(eps_macro_prediction(:,1), eps_macro_validation(:,1));
+str = sprintf('r= %1.4f',cor(1,2));
 T = text(max(get(gca, 'xlim'))*0.2, max(get(gca, 'ylim'))*0.8, str); 
 set(T, 'fontsize', 16, 'verticalalignment', 'top', 'horizontalalignment', 'left');
-legend([char(949),'_{xx,CNN}^{M} vs.',char(949),'_{xx, SCA}^{M}'],[char(949),'_{xx,SCA}^{M} vs.',char(949),'_{xx, SCA}^{M}'],'Location','southeast')
+legend([char(949),'_{xx,CNN}^{M} vs.',char(949),'_{xx, SCA}^{M}'],...
+       [char(949),'_{xx,SCA}^{M} vs.',char(949),'_{xx, SCA}^{M}'], 'Location', 'southeast')
 set(gca,'FontSize',fs);
-xlabel([char(949),'_{xx,CNN}^{M}'],'fontsize',16); ylabel([char(949),'_{xx,SCA}^{M}'],'fontsize',16);
-xlim([0 0.05]);ylim([0 0.05])
-ax = gca;ax.FontSize = 16;
+xlabel([char(949),'_{xx,CNN}^{M}'], 'fontsize', 16);
+ylabel([char(949),'_{xx,SCA}^{M}'], 'fontsize', 16);
+xlim([0 0.05])
+ylim([0 0.05])
+ax = gca;
+ax.FontSize = 16;
+
 
 subplot(1,3,2);
-plot(eps_macro_prediction(:,2),eps_macro_validation(:,2),'blue*');hold on
-plot(eps_macro_validation(:,2),eps_macro_validation(:,2),'k')
-cor=corrcoef(eps_macro_prediction(:,2),eps_macro_validation(:,2));
-str=sprintf('r= %1.4f',cor(1,2));
+plot(eps_macro_prediction(:,2),eps_macro_validation(:,2), 'blue*');
+hold on
+plot(eps_macro_validation(:,2),eps_macro_validation(:,2), 'k')
+
+cor = corrcoef(eps_macro_prediction(:,2), eps_macro_validation(:,2));
+str = sprintf('r= %1.4f',cor(1,2));
 T = text(max(get(gca, 'xlim'))*0.2, max(get(gca, 'ylim'))*0.8, str); 
 set(T, 'fontsize', 16, 'verticalalignment', 'top', 'horizontalalignment', 'left');
-legend([char(949),'_{yy,CNN}^{M} vs.',char(949),'_{yy, SCA}^{M}'],[char(949),'_{yy,SCA}^{M} vs.',char(949),'_{yy, SCA}^{M}'],'Location','southeast')
+legend([char(949),'_{yy,CNN}^{M} vs.',char(949),'_{yy, SCA}^{M}'],...
+       [char(949),'_{yy,SCA}^{M} vs.',char(949),'_{yy, SCA}^{M}'], 'Location', 'southeast')
 set(gca,'FontSize',fs);
-xlabel([char(949),'_{yy,CNN}^{M}'],'fontsize',16); ylabel([char(949),'_{yy,SCA}^{M}'],'fontsize',16);
-xlim([0 0.05]);ylim([0 0.05])
-ax = gca;ax.FontSize = 16;
+xlabel([char(949),'_{yy,CNN}^{M}'], 'fontsize', 16);
+ylabel([char(949),'_{yy,SCA}^{M}'], 'fontsize', 16);
+xlim([0 0.05])
+ylim([0 0.05])
+ax = gca;
+ax.FontSize = 16;
+
 
 subplot(1,3,3);
-plot(eps_macro_prediction(:,3),eps_macro_validation(:,3),'g*');hold on
+plot(eps_macro_prediction(:,3),eps_macro_validation(:,3),'g*');
+hold on
 plot(eps_macro_validation(:,3),eps_macro_validation(:,3),'k')
-cor=corrcoef(eps_macro_prediction(:,3),eps_macro_validation(:,3));
-str=sprintf('r= %1.4f',cor(1,2));
+
+cor = corrcoef(eps_macro_prediction(:,3), eps_macro_validation(:,3));
+str = sprintf('r= %1.4f',cor(1,2));
 T = text(max(get(gca, 'xlim'))*0.2, max(get(gca, 'ylim'))*0.8, str); 
 set(T, 'fontsize', 16, 'verticalalignment', 'top', 'horizontalalignment', 'left');
-legend([char(947),'_{xy,CNN}^{M} vs.',char(947),'_{xy, SCA}^{M}'],[char(947),'_{xy,SCA}^{M} vs.',char(947),'_{xy, SCA}^{M}'],'Location','southeast')
+legend([char(947),'_{xy,CNN}^{M} vs.',char(947),'_{xy, SCA}^{M}'],...
+       [char(947),'_{xy,SCA}^{M} vs.',char(947),'_{xy, SCA}^{M}'], 'Location', 'southeast')
 set(gca,'FontSize',fs);
-xlabel([char(947),'_{xy,CNN}^{M}'],'fontsize',16); ylabel([char(947),'_{xy,SCA}^{M}'],'fontsize',16);
-xlim([0 0.05]);ylim([0 0.05])
-ax = gca;ax.FontSize = 16;
+xlabel([char(947),'_{xy,CNN}^{M}'], 'fontsize', 16);
+ylabel([char(947),'_{xy,SCA}^{M}'], 'fontsize', 16);
+xlim([0 0.05])
+ylim([0 0.05])
+ax = gca;
+ax.FontSize = 16;
